661e4d19ced548791f225ec4ec7e9ee263fbaab4
Adryan Ryan <adryanryan.s.g@icloud.com>
2025-04-19 18:30:37 -0300

    Feat: Trantamento de erro Repository

--- src/modules/task/TaskRepository.ts
+++ src/modules/task/TaskRepository.ts
@@ -1,16 +1,40 @@
-import { TaskCreated } from "./../types/task.d";
-import { prisma } from "../lib/prisma";
+import { TaskCreated } from "@/types/task.d";
+import { prisma } from "@/lib/prisma";
 
-export function getTasks() {
-  const result = prisma.task.findMany();
-  return result;
+// Função para obter todas as tarefa(
+export async function getTasks() {
+  try {
+    const result = prisma.task.findMany();
+    return result;
+  } catch (error) {
+    throw new Error("Erro ao obeter as tarefas");
+  }
 }
 
-export function getById(id: string) {
-  const result = prisma.task.findUnique({ where: { id } });
-  return result;
+// Função para obter uma tarefa por ID
+export async function getById(id: string) {
+  try {
+    const result = prisma.task.findUnique({ where: { id } });
+    if (!result) {
+      throw new Error("Tarefa não encontrada");
+    }
+    return result;
+  } catch (error) {
+    throw new Error(`Erro ao obter tarefa com ID ${id}`);
+  }
 }
 
-export function createTasks(task: TaskCreated) {
-  const result = prisma.task.create({ data: { task } });
+// Função  para criar uma nova tarefa
+export async function createTasks(task: TaskCreated) {
+  try {
+    const result = prisma.task.create({
+      data: {
+        title: task.title,
+        description: task.description,
+        status: task.status,
+      },
+    });
+  } catch (error) {
+    throw new Error("Erro ao criar Tarefa");
+  }
 }
