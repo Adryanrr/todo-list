17df49e3b10d8b9322604a344442407d54f743cb
Adryan Ryan <adryanryan.s.g@icloud.com>
2025-04-20 12:53:21 -0300

    Feat: Configurando server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import Fastify, { fastify } from "fastify";
+import { taskRoutes } from "@/modules/task/RouteTask";
 
 const app = fastify({
   logger: true,
+});
+
+app.register(taskRoutes);
+
+app.listen({ port: 3333 }, () => {
+  console.log("Servidor rodando em http://localhost:3333");
 });
